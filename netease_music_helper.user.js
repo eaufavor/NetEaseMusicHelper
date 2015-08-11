e5ae415b110c67856b67c4df42651c9d4bcf0f17
Bob <shixiaobao17145@163.com>
2015-08-11 15:47:26 +0800

    create project and add files

---
+++ netease_music_helper.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name       网易云音乐海外助手
+// @namespace    http:/sudodev.cn/
+// @version      0.1
+// @description  让你在海外正常听网易云音乐
+// @author       Bob
+// @match        http://music.163.com/*
+// @grant        none
+// ==/UserScript==
+window.GAbroad=false;
+/**
+如果是在浏览器控制台，你也可以把这句代码：window.GAboard=window.contentFrame.GAbroad = false; 执行，目前来说，可以达到相同的效果
+**/
